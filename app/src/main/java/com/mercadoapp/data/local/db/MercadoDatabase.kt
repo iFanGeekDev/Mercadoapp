244bfcb9f3b8632aa05f0bd3f518e7edbee31b88
Diego <diego@mercadoapp.dev>
2026-05-09 17:23:30 -0500

    fix: increment room database version to 3

--- app/src/main/java/com/mercadoapp/data/local/db/MercadoDatabase.kt
+++ app/src/main/java/com/mercadoapp/data/local/db/MercadoDatabase.kt
@@ -9,7 +9,7 @@ import com.mercadoapp.data.local.entity.ProductEntity
 
 @Database(
     entities = [ProductEntity::class, CartItemEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class MercadoDatabase : RoomDatabase() {
